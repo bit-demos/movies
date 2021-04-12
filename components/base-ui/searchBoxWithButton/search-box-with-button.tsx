1a230de1e0795d38c38a6993970f2bdf8e6c5ff5
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2021-04-12 11:40:16 +0300

    fix searchwithbutton

--- components/base-ui/searchBoxWithButton/search-box-with-button.tsx
+++ components/base-ui/searchBoxWithButton/search-box-with-button.tsx
@@ -1,7 +1,6 @@
 // @ts-ignore
 import React, {useState, useEffect} from 'react';
 import cs from 'classNames';
-import bootstrapStyles from '@teambit/movie-demo.styling.bootstrap-override';
 import {Input} from '@teambit/movie-demo.base-ui.input';
 import {PrimaryButton} from '@teambit/movie-demo.base-ui.button';
 
@@ -27,14 +26,14 @@ export const SearchBoxWithButton = ({onClicked, buttonText, inputPlaceholder}: S
                 styles.search
             )}>
                 <Input
-                    className={cs(bootstrapStyles["mr-2"], bootstrapStyles["col-md-8"], bootstrapStyles["align-self-stretch"])}
+                    className={cs(styles.input)}
                     onInput={e => setInputValue(e.currentTarget.value)}
                     placeHolder={inputPlaceholder || "Enter text"}
                     value={inputValue}
                     onChange={() => {
                     }}
                 />
-                <PrimaryButton type="submit" className={cs(bootstrapStyles["text-truncate"])}
+                <PrimaryButton type="submit" className={cs(styles.button)}
                                onClick={() => handleClick()}>{buttonText || "Submit"}</PrimaryButton>
             </div>
 
