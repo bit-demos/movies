12940ae6572f328a7f6c41445671ba26537a15b3
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2021-04-27 20:32:11 +0300

    convert use-search-movie to using api-hook-factory

--- components/movies/data/hooks/use-search-movies/use-search-movies.ts
+++ components/movies/data/hooks/use-search-movies/use-search-movies.ts
@@ -1,52 +1,21 @@
 import {useState, useEffect} from 'react';
 import { useContextProvider, ContextResource } from '@learn-harmony/movies.context.api-context-provider';
-import { Movie, MovieFromApi, MovieResponse } from '@learn-harmony/movies.movies.model.movie';
+import { Movie, MovieFromApi, MovieResponse } from '@learn-harmony/movies.models.movie';
+import { ApiHookFactory } from '@learn-harmony/movies.utils.api-hook-factory';
 
-export const useSearchMovies = (): [
-  (searchFor: string) => Promise<void>,
-  Movie[],
-  boolean,
-  string,
-] => {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
+export const useSearchMovies = () => {
 
-  const [searchResult, searchMovies] = useContextProvider((searchStr: string): ContextResource<MovieResponse> => ({
+  const apiContext = (searchStr: string): ContextResource<MovieResponse> => ({
       params: {
         s: searchStr
       }
-    })
-  );
+    });
 
-  useEffect(() => handleDataReturn(), [searchResult])
-
-  const handleDataReturn = () => {
-    if (searchResult.isLoading) {
-      setIsLoading(true);
-      return;
-    }
-    if (!searchResult.data) return;
-    const { data }  = searchResult;
-    const list = data.Search && data.Search.length ? data.Search.map(
+    const processData = (data) =>  {
+       return data.Search && data.Search.length ? data.Search.map(
       (m: MovieFromApi) => Movie.fromApiObject(m)
     ) : [];
-    setMovies(list);
-    if (error) setError('');
-    setIsLoading(false);
-  }
-
-  const getMovies = async (searchFor: string) => {
-    if (!searchFor) return;
-
-    setIsLoading(true);
-    try {
-      searchMovies(searchFor);
-    } catch (err) {
-      setError(err.toString());
-      setIsLoading(false);
     }
-  };
 
-  return [getMovies, movies, isLoading, error];
+  return ApiHookFactory(apiContext, processData);
 };
