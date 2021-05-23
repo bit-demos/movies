5a3f22001e4646dd1cf5bb1f7d9c35b2f389148a
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2021-05-23 22:23:26 +0300

    improve tests

--- components/base-ui/searchBoxWithButton/search-box-with-button.spec.tsx
+++ components/base-ui/searchBoxWithButton/search-box-with-button.spec.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {render, getByPlaceholderText} from '@testing-library/react';
-import { SimpleSearch, SearchBoxWithPlaceholder } from './search-box-with-button.compositions';
+import {render} from '@testing-library/react';
+import { SearchBoxWithPlaceholder } from './search-box-with-button.compositions';
 import { SearchBoxWithButton } from './search-box-with-button';
-import styles from './search-box-with-button.module.scss';
 
 describe('Search Box With Button component', () => {
     it('renders correctly', () => {
